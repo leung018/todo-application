de04b6f05aa48c494d7de8fb851b0ddba6f84dee
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-22 22:17:56 +0800

    Add test for multiple listing

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -18,4 +18,19 @@ describe('ExpressApp', () => {
     expect(duties[0].name).toBe('My Duty')
     expect(typeof duties[0].id).toBe('string')
   })
+
+  it('should create multiple duties and list them', async () => {
+    const app = ExpressAppInitializer.createNull().app
+    await request(app).post('/duties').send({ name: 'Duty 1' })
+    await request(app).post('/duties').send({ name: 'Duty 2' })
+    await request(app).post('/duties').send({ name: 'Duty 3' })
+
+    const response = await request(app).get('/duties')
+
+    const duties = response.body
+    expect(duties.length).toBe(3)
+    expect(duties[0].name).toBe('Duty 1')
+    expect(duties[1].name).toBe('Duty 2')
+    expect(duties[2].name).toBe('Duty 3')
+  })
 })
