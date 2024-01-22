0e4fad0ff2f48d2874e67cddbef862da24b7c4f1
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-22 17:09:27 +0800

    Add failing test for id of duty

--- backend/src/models/duty.test.ts
+++ backend/src/models/duty.test.ts
@@ -7,4 +7,10 @@ describe('DutyFactory', () => {
     expect(duty.name).toBe('Duty 1')
     expect(typeof duty.id).toBe('string')
   })
+
+  it('should duties be created is different id', () => {
+    const duty1 = DutyFactory.createDuty({ name: 'Duty 1' })
+    const duty2 = DutyFactory.createDuty({ name: 'Duty 2' })
+    expect(duty1.id).not.toBe(duty2.id)
+  })
 })
