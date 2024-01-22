8b96851ead7ed829e19b2198e4fdccc220108015
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-22 22:37:54 +0800

    Refactor to helper functions in tests

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -1,36 +1,44 @@
-import { describe, it, expect } from '@jest/globals'
+import { describe, it, expect, beforeEach } from '@jest/globals'
 import request from 'supertest'
 import { ExpressAppInitializer } from './app'
+import { Express } from 'express'
 
 describe('ExpressApp', () => {
+  let app: Express
+
+  beforeEach(() => {
+    app = ExpressAppInitializer.createNull().app
+  })
+
   it('should create single duty and list it', async () => {
-    const app = ExpressAppInitializer.createNull().app
-    const response = await request(app)
-      .post('/duties')
-      .send({ name: 'My Duty' })
-    expect(response.status).toBe(201)
+    await createDuty({ name: 'My Duty' })
 
-    const response2 = await request(app).get('/duties')
-    expect(response2.status).toBe(200)
-
-    const duties = response2.body
+    const duties = await listDuties()
     expect(duties.length).toBe(1)
     expect(duties[0].name).toBe('My Duty')
     expect(typeof duties[0].id).toBe('string')
   })
 
   it('should create multiple duties and list them', async () => {
-    const app = ExpressAppInitializer.createNull().app
-    await request(app).post('/duties').send({ name: 'Duty 1' })
-    await request(app).post('/duties').send({ name: 'Duty 2' })
-    await request(app).post('/duties').send({ name: 'Duty 3' })
+    await createDuty({ name: 'Duty 1' })
+    await createDuty({ name: 'Duty 2' })
+    await createDuty({ name: 'Duty 3' })
 
-    const response = await request(app).get('/duties')
-
-    const duties = response.body
+    const duties = await listDuties()
     expect(duties.length).toBe(3)
     expect(duties[0].name).toBe('Duty 1')
     expect(duties[1].name).toBe('Duty 2')
     expect(duties[2].name).toBe('Duty 3')
   })
+
+  async function createDuty({ name = 'Name of Duty' } = {}) {
+    const response = await request(app).post('/duties').send({ name })
+    expect(response.status).toBe(201)
+  }
+
+  async function listDuties() {
+    const response = await request(app).get('/duties')
+    expect(response.status).toBe(200)
+    return response.body
+  }
 })
