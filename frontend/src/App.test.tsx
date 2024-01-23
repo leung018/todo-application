2e1172284f733e22b49f5c06146a74cf098014cb
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-23 19:51:44 +0800

    Fix the error of matchMedia

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,6 +2,19 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
+  beforeAll(() => {
+    // Ref: https://github.com/ant-design/ant-design/issues/21096#issuecomment-732368647
+    Object.defineProperty(window, 'matchMedia', {
+      value: () => {
+        return {
+          matches: false,
+          addListener: () => {},
+          removeListener: () => {},
+        }
+      },
+    })
+  })
+
   it('dummy test', () => {
     // Just check the jest is working. This test will be removed.
     render(<App />)
