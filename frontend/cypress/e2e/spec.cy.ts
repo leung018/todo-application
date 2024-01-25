e66cdcba5c0ef542c6bee98a6cba0ff004f47239
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-25 14:04:04 +0800

    Add cypress test

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('e2e tests', () => {
+  it('should create duty successfully', () => {
+    cy.visit('/')
+
+    cy.findByPlaceholderText('Add new duty').type('Duty 1')
+    cy.findByText('Add').click()
+
+    cy.findByText('Duty 1').should('exist')
+
+    cy.visit('/')
+    cy.findByText('Duty 1').should('exist')
   })
 })
