07894d75c43002285808c59588ae7430951202f4
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-25 21:07:54 +0800

    Also update default to 8080 for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import { env } from 'process'
 import { ExpressAppInitializer } from './app'
 
 const app = ExpressAppInitializer.create().app
-const port = env.PORT || 3001
+const port = env.PORT || 8080
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
