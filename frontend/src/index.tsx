0f6c80d75221916b04aaf63b65857ff51d4c0de0
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-25 20:57:58 +0800

    Add docker compose for frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,9 +5,12 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { DutyRemoteServiceImpl } from './services/duty'
 
+const apiEndpoint =
+  process.env.REACT_APP_API_ENDPOINT || 'http://localhost:3001'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const dutyRemoteService = new DutyRemoteServiceImpl({
-  apiEndpoint: 'http://localhost:3001', // TODO: Use env variable
+  apiEndpoint,
 })
 root.render(
   <React.StrictMode>
