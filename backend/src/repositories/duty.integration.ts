ffbc18230eafb6a69959d65f8906caeec9eff6b2
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-26 20:54:05 +0800

    Add command to run integration tests

---
+++ backend/src/repositories/duty.integration.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from '@jest/globals'
+
+describe('PostgresDutyRepository', () => {
+  it('dummy test to test this file can be run', () => {
+    expect(true).toBe(true)
+  })
+})
