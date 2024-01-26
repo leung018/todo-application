67b2c6fd47adcf230695171fdc2ef9266851c7ab
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-26 19:03:51 +0800

    Remove reportWebVitals

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 import { DutyRemoteServiceImpl } from './services/duty'
 
 const apiEndpoint =
@@ -17,8 +16,3 @@ root.render(
     <App dutyRemoteService={dutyRemoteService} />
   </React.StrictMode>,
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
