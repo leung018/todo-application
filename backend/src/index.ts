3480a75f6d4a1d0a6adc512176d93f2848ec75e7
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-28 00:07:05 +0800

    Add error log

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,8 +6,13 @@ const port = env.PORT || 8080
 
 ExpressAppInitializer.create({
   postgresContext: newPostgresContextFromEnv(),
-}).then((initializer) => {
-  initializer.app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-  })
 })
+  .then((initializer) => {
+    initializer.app.listen(port, () => {
+      console.log(`Server running on port ${port}`)
+    })
+  })
+  .catch((err) => {
+    console.error('Error encountered when starting server', err)
+    process.exit(1)
+  })
