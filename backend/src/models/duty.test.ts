56734924ca6a8b8e108c44b0633723a53c305898
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-28 11:10:46 +0800

    Validate empty name

--- backend/src/models/duty.test.ts
+++ backend/src/models/duty.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from '@jest/globals'
 import { DutyFactory } from './duty'
+import { InvalidArgumentError } from '../utils/errors'
 
 describe('DutyFactory', () => {
   it('should duty be created', () => {
@@ -12,6 +13,11 @@ describe('DutyFactory', () => {
     const duty1 = createDuty()
     const duty2 = createDuty()
     expect(duty1.id).not.toBe(duty2.id)
+  })
+
+  it('should prevent empty name', () => {
+    expect(() => createDuty({ name: '' })).toThrow(InvalidArgumentError)
+    expect(() => createDuty({ name: ' ' })).toThrow(InvalidArgumentError)
   })
 
   function createDuty({ name = 'Name of Duty' } = {}) {
--- backend/src/models/duty.ts
+++ backend/src/models/duty.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
+import { InvalidArgumentError } from '../utils/errors'
 
 export interface Duty {
   readonly id: string
@@ -7,6 +8,10 @@ export interface Duty {
 
 export class DutyFactory {
   static createDuty({ name }: { name: string }): Duty {
+    if (name.trim().length === 0) {
+      throw new InvalidArgumentError('Name of duty cannot be empty')
+    }
+
     return {
       id: uuidv4(),
       name,
