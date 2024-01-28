5f252d411354ca92b08ac99941471381d9ba8be0
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-28 19:01:17 +0800

    Add non ok response handling

--- frontend/src/services/duty.ts
+++ frontend/src/services/duty.ts
@@ -19,7 +19,16 @@ export class DutyRemoteServiceImpl implements DutyRemoteService {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ name }),
-    }).then((res) => res.json())
+    }).then((res) => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        // TODO: test it in e2e test
+        return res.json().then((body) => {
+          throw new Error(body.message)
+        })
+      }
+    })
   }
 
   async listDuties() {
