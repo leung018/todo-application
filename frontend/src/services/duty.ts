cdd18858d6e17cbed63228e1de6b6f264aa5c927
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-29 21:16:50 +0800

    Update remoteService

--- frontend/src/services/duty.ts
+++ frontend/src/services/duty.ts
@@ -3,6 +3,7 @@ import { Duty } from '../models/duty'
 export interface DutyRemoteService {
   createDuty: (name: string) => Promise<Duty>
   listDuties(): Promise<Duty[]>
+  updateDuty: (duty: Duty) => Promise<unknown>
 }
 
 export class DutyRemoteServiceImpl implements DutyRemoteService {
@@ -30,6 +31,16 @@ export class DutyRemoteServiceImpl implements DutyRemoteService {
     })
   }
 
+  async updateDuty(duty: Duty) {
+    return fetch(`${this.apiEndpoint}/duties/${duty.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: duty.name }),
+    })
+  }
+
   async listDuties() {
     return fetch(`${this.apiEndpoint}/duties`).then((res) => res.json())
   }
@@ -39,12 +50,20 @@ export class InMemoryDutyService implements DutyRemoteService {
   private readonly duties: Duty[] = []
 
   async createDuty(name: string) {
-    const duty = { id: 'dummy', name } // TODO: Replace dummy id with sth dynamic
+    const duty = { id: `${this.duties.length}`, name }
     this.duties.push(duty)
     return duty
   }
 
   async listDuties() {
     return this.duties
+  }
+
+  async updateDuty(duty: Duty) {
+    const index = this.duties.findIndex((d) => d.id === duty.id)
+    if (index === -1) {
+      throw new Error('Duty not found')
+    }
+    this.duties[index] = duty
   }
 }
