52839e2a68eca56addd30790c80cefdc632be414
Leung Cheng <monewckl@hotmail.com.hk>
2024-01-30 01:42:07 +0800

    Refactor into createDuty command

--- e2e/cypress/e2e/spec.cy.js
+++ e2e/cypress/e2e/spec.cy.js
@@ -6,8 +6,7 @@ describe('e2e tests', () => {
   it('should create duty successfully', () => {
     cy.visit('/')
 
-    cy.findByPlaceholderText('Add new duty').type('Duty 1')
-    cy.findByText('Add').click()
+    cy.createDuty({ name: 'Duty 1' })
 
     cy.findByText('Duty 1').should('exist')
 
@@ -26,8 +25,7 @@ describe('e2e tests', () => {
   it('should update created duty', () => {
     cy.visit('/')
 
-    cy.findByPlaceholderText('Add new duty').type('Duty 1')
-    cy.findByText('Add').click()
+    cy.createDuty({ name: 'Duty 1' })
 
     cy.findByTestId('edit-button-0').click()
     cy.findByDisplayValue('Duty 1').type(' Updated')
@@ -42,8 +40,7 @@ describe('e2e tests', () => {
   it('should display error message when updating duty to empty', () => {
     cy.visit('/')
 
-    cy.findByPlaceholderText('Add new duty').type('Duty 1')
-    cy.findByText('Add').click()
+    cy.createDuty({ name: 'Duty 1' })
 
     cy.findByTestId('edit-button-0').click()
     cy.findByDisplayValue('Duty 1').clear()
